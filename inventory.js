54409541631ff6a6724188b4a3fb5d10c1f31803
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-23 23:37:38 -0500

    Ability to search by name

--- inventory.js
+++ inventory.js
@@ -26,9 +26,9 @@ var searchInventory = function(){
 
   for ( i=0 ; i<inventory.length ; i++ ){
     if ((selectedSize=='No Preference' || inventory[i].itemSize==selectedSize) &&
-      (selectedColor=='No Preference' || inventory[i].itemColor==selectedColor)) {
-        // push match into results array
-        searchResults.push(inventory[i]);
+    (selectedColor=='No Preference' || inventory[i].itemColor==selectedColor)) {
+      // push match into results array
+      searchResults.push(inventory[i]);
     } // end match
   } // end for
 
@@ -37,8 +37,8 @@ var searchInventory = function(){
   console.log( 'We found',searchResults.length, 'matches!');
   if (searchResults.length>0){
     console.log('This is what we have in stock:');
-    for (i = 0; i < inventory.length; i++) {
-      console.log('A',inventory[i].itemSize+',',inventory[i].itemColor,inventory[i].itemName+'!');
+    for (i = 0; i < searchResults.length; i++) {
+      console.log('A',searchResults[i].itemSize+',',searchResults[i].itemColor,searchResults[i].itemName+'!');
     } // end listing for loop
   } else {
     console.log('Sorry, we currently don\'t have anything in stock matching those selections.');
@@ -57,13 +57,48 @@ var stockNewItem = function(){
   var newColor = document.getElementById('newItemColor').value;
   var newName = document.getElementById('newItemName').value;
 
-  if (newSize != '' && newColor != '' && newName != '') {
+  if (newSize !== 'Choose a Size' && newColor !== 'Choose a Color' && newName !== '') {
     stockItem(newSize, newColor, newName);
     console.log('A brand new', newSize+',', newColor, newName, 'has been added to our inventory!');
   } else {
     console.log('Sorry! You need to enter values in all three fields, please.');
   }
 }; // end stockNewItem
+
+var searchInventoryByName = function(){
+  console.log('in searchInventoryByName!');
+  // initialize local variables
+  var searchResults = [];
+
+  // get info from user
+  nameToSearchFor = document.getElementById('nameIn').value;
+  console.log('Name to search for:',nameToSearchFor+"!");
+  // check if an item in stock has this name
+
+  if (nameToSearchFor === '') {
+    console.log('Sorry! You need to enter a value in the field, please.');
+  } else {
+
+    for ( i=0 ; i<inventory.length ; i++ ){
+      if (inventory[i].itemName===nameToSearchFor) {
+        // push match into results array
+        searchResults.push(inventory[i]);
+      } // end match
+    } // end for
+
+    // Display results
+    console.log( 'You searched for items with this name:',nameToSearchFor);
+    console.log( 'We found',searchResults.length, 'matches!');
+    if (searchResults.length>0){
+      console.log('This is what we have in stock:');
+      for (i = 0; i < searchResults.length; i++) {
+        console.log('A',searchResults[i].itemSize+',',searchResults[i].itemColor,searchResults[i].itemName+'!');
+      } // end listing for loop
+    } else {
+      console.log('Sorry, we currently don\'t have anything in stock matching those selections.');
+    }
+  }
+}; // end searchInventoryByName
 
 /// ADD ITEMS ///
 // stockItem( '', '', '');
