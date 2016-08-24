12e9b59442984037baf6d6d4f5aa3feb83227212
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-23 22:59:16 -0500

    Updates from Day 2 in-class work

--- inventory.js
+++ inventory.js
@@ -1,5 +1,6 @@
 console.log('inventory.js sourced!');
 // global variables
+var i;
 var inventory = [];
 var selectedColor = 'No Preference';
 var selectedSize = 'No Preference';
@@ -9,10 +10,13 @@ function ItemInStock(size, color, name){
   this.itemSize = size;
   this.itemColor = color;
   this.itemName = name;
-}
+} // end ItemInStock
 
 var searchInventory = function(){
   console.log( 'in searchInventory ');
+  // initialize local variables
+  var searchResults = [];
+
   // get info from user
   selectedColor = document.getElementById('colorIn').value;
   selectedSize = document.getElementById('sizeIn').value;
@@ -21,14 +25,25 @@ var searchInventory = function(){
   // check if an item fits this description
 
   for ( i=0 ; i<inventory.length ; i++ ){
-    if (selectedSize=='No Preference' || inventory[i].itemSize==selectedSize) {
-      if (selectedColor=='No Preference' || inventory[i].itemColor==selectedColor) {
-        console.log(inventory[i].itemName);
-      }
-    }
-  }
+    if ((selectedSize=='No Preference' || inventory[i].itemSize==selectedSize) &&
+      (selectedColor=='No Preference' || inventory[i].itemColor==selectedColor)) {
+        // push match into results array
+        searchResults.push(inventory[i]);
+    } // end match
+  } // end for
 
-};
+  // Display results
+  console.log( 'You searched for items with',selectedSize,'size and',selectedColor,'color:');
+  console.log( 'We found',searchResults.length, 'matches!');
+  if (searchResults.length>0){
+    console.log('This is what we have in stock:');
+    for (i = 0; i < inventory.length; i++) {
+      console.log('A',inventory[i].itemSize+',',inventory[i].itemColor,inventory[i].itemName+'!');
+    } // end listing for loop
+  } else {
+    console.log('Sorry, we currently don\'t have anything in stock matching those selections.');
+  }
+}; // end searchInventory
 
 var stockItem = function ( size, color, name ){
   console.log('In stockItem with:', size, color, name );
@@ -41,7 +56,10 @@ var stockItem = function ( size, color, name ){
 // stockItem( '', '', '');
 stockItem( 'Small', 'Blue', 'Smurf');
 stockItem( 'Medium', 'Bus Seat Green', 'Baby Hulk');
+stockItem( 'Large', 'Mermaid Treasure', 'Prime Academy');
 stockItem( 'Small', 'Purple', 'Eggplant');
 stockItem( 'Medium', 'Purple', 'Prince');
+stockItem( 'Small', 'Yellow Canary', 'Brain');
+stockItem( 'Small', 'Yellow Canary', 'Lemon');
 
 console.log(inventory);
